930c881336bcb3a18cf2c13c546b71e690cd6ca1
TAN5452-MD <seesawgzz@gmail.com>
2023-04-22 15:57:29 +0800

    1

---
+++ 02_es6proxy和reflect/Promise/Promise的基本使用.js
@@ -0,0 +1,27 @@
+
+
+//传入的这个函数被称之为executor执行器函数
+//resolve 在成功时的回调
+//reject 在失败时的回调
+const promise = new Promise((
+    resolve,
+    reject
+)=>{
+    console.log('promise传入的函数被执行了')
+})
+
+//resolve
+promise.then(()=>{
+
+})
+//reject
+promise.catch(()=>{
+
+})
+
+//也可以这样写
+promise.then((res)=>{
+    console.log(res);
+},(err)=>{
+    console.log(err);
+})
