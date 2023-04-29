1741d02753fd79ff9f58d77402a6211427a9b5be
seesaw <545243476@qq.com>
2023-04-30 00:06:05 +0800

    1

--- 02_es6proxy和reflect/Promise/13_promise_catch方法.js
+++ 02_es6proxy和reflect/Promise/13_promise_catch方法.js
@@ -52,62 +52,38 @@ class myPromise {
         }
     }
     then(onFulfilled,onReject){
+        //如果为空就手动抛出异常 这样第二个promise就能捕捉到
+        onReject = onReject ?? (err => {throw err})
+
         //为了实现链式调用 需要返回promise
         return new myPromise((resolve,reject) => {
         //如果此时的状态已经确定的话就立即执行
         if(this.status === PROMISE_STATUS_FULFILLED && onFulfilled){
-            //拿到返回值并继续执行
-            /**
-             * try {
-             *   const result = onFulfilled(this.value)
-             *     resolve(result)
-             * } catch (error) {
-             *     reject(error)
-             * }
-             */
-                execFunctionWithCatchError(onFulfilled, this.value, resolve, reject)
+        execFunctionWithCatchError(onFulfilled, this.value, resolve, reject)
         }
         if(this.status === PROMISE_STATUS_REJECT && onReject){
-            /**
-             * try {
-             * const result = onReject(this.value)
-             * resolve(result)
-             * } catch (error) {
-             *     reject(error)
-             * }
-             */
+        
         execFunctionWithCatchError(onReject, this.value, resolve, reject)
 
         }
         //如果状态未确定的时候才添加
         if(this.status === PROMISE_STATUS_PENDINGI){
         //由于需要拿到返回值再reslove 所以这里用回调函数处理一下
-        this.onFulfilledFns.push(()=>{
-            /**
-             * try{
-             * const value = onFulfilled(this.value)
-             * resolve(value)
-             * }catch(error){
-             *     reject(error)
-             * }
-             */
+       onFulfilled &&  this.onFulfilledFns.push(()=>{
+           
             execFunctionWithCatchError(onFulfilled, this.value, resolve, reject)
         }) 
-        this.onRejectFns.push(()=>{
-            /**
-             * try{
-             * const reason = onReject(this.reason)
-             * resolve(reason)
-             * }catch(error){
-             * reject(error)
-             * }
-             */
+        onReject && this.onRejectFns.push(()=>{
             execFunctionWithCatchError(onReject,this.reason, resolve, reject)
         })
         } 
     })
     }
-}
+    catch(onReject){
+        return this.then(null,onReject)
+    }
+    }
+
 
 const promise = new myPromise((resolve,reject)=>{
    resolve("123")
@@ -115,17 +91,8 @@ const promise = new myPromise((resolve,reject)=>{
     // throw new Error("789")
 })
 
-promise
-.then(res=>{
-  console.log(res);
-  //return ("11")
- throw new Error("123")
-},err=>{
- console.log(err);
-    //return 111
-})
-.then(res=>{
+promise.then(res=>{
     console.log(res);
-},err=>{
-    console.log(err);})
-    
+}).catch(err=>{
+
+})
